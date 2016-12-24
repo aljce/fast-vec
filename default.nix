a350163fe9d398702caa832988b63fbc7c5e9667
Kyle McKean <mckean.kylej@gmail.com>
2016-12-24 11:21:18 -0500

    Initial commit

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, stdenv }:
+      mkDerivation {
+        pname = "fast-vec";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [ base ];
+        description = "Length indexed vectors with identical performance to vector";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
